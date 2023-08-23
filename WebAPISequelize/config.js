9e9c5e6e4c63ce2d1209571603140ccb1ca29e43
Guillermo Echichure <gechichure@hotmail.com>
2023-08-23 00:25:40 -0300

    Se quita archivo config.js

--- WebAPISequelize/config.js
+++
@@ -1,9 +0,0 @@
-export const databaseConfig = {
-    dialect: 'mysql',
-    host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'goose710',
-    database: process.env.DB_DATABASE || 'node_rest_api',
-};
-
-export const portForHTTP = process.env.PORT || 3000;
