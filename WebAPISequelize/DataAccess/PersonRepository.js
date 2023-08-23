c34cfb799454df8921d66fca04eb96c3e78ca690
Guillermo Echichure <gechichure@hotmail.com>
2023-08-23 10:05:18 -0300

    .gitignore update

--- WebAPISequelize/DataAccess/PersonRepository.js
+++ WebAPISequelize/DataAccess/PersonRepository.js
@@ -1,5 +1,5 @@
 import {DataTypes, Sequelize} from 'sequelize';
-import {databaseConfig}   from '../config.js';
+import {databaseConfig} from '../config.js';
 
 
 const sequelize = new Sequelize(databaseConfig);
