c393f66ad135d7d81dbf25f25871effbf151270d
Guillermo Echichure <gechichure@hotmail.com>
2023-09-10 14:14:22 -0300

    App.js update
    - Se modifica codigo de app.js, se agrega la conexion a la bd dese alli utilizando la clase MyContext.
    - Se abstrae el puerto al archivo config.js.
    - Se corre y se logra conectar el servidor express + el servidor mongoDB desde app.js.

--- WebAPIMongoDB/WebAPI/app.js
+++ WebAPIMongoDB/WebAPI/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const config = require('../properties/config');
 const personController = require('./controllers/PersonController');
+const mongoose = require('mongoose');
+
+
+
+// Conexión a MongoDB desde MyContext.js
+mongoose.connect(config.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Conexión a MongoDB establecida correctamente');
+    })
+    .catch((error) => {
+        console.error('Error de conexión a MongoDB:', error);
+        process.exit(1); // Salir del proceso con un código de error
+    });
 
 app.use(express.json());
-
 app.use('/api', personController);
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
