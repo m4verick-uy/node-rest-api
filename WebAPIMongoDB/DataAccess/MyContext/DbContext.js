3836bc54d069024bd8018930fcb39408d5596e86
Guillermo Echichure <gechichure@hotmail.com>
2023-09-10 12:52:36 -0300

    Se agrega proyecto WebAPIMongoDB
    - Se crear correctamente base de datos mongoDBTest
    - Se crea archivo config.js y no se sube al repo.
    - Se crea clase MyContext manejando la conexión y creando la colección persons.

--- WebAPIMongoDB/DataAccess/MyContext/DbContext.js
+++ WebAPIMongoDB/DataAccess/MyContext/DbContext.js
@@ -1,15 +1,45 @@
-const uri =
-    "<Your Atlas Connection String>";
+const mongoose = require('mongoose');
+const config = require('../../Properties/config');
 
+const url = config.mongoURL;
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
 
-const db = mongoose.connection;
+mongoose.connect(url, options)
+    .then(async () => {
+        console.log('Conexión a MongoDB establecida correctamente');
 
-db.once('open', ()=> {
-    console.log('Se conecto :) !!!');
-})
+        // Crear una nueva colección (tabla) llamada "miColeccion"
+        const PersonaSchema = new mongoose.Schema({
+            nombre: String,
+            edad: Number,
+            email: String,
+            // Define otros campos según tus necesidades
+        });
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        // Crea el modelo basado en el esquema
+        const persons = mongoose.model('persons', PersonaSchema);
 
-module.exports = db;
+        // Utiliza el modelo para interactuar con la colección
+        const nuevoDocumento = new persons({
+            nombre: 'Valor1',
+            edad: 42,
+            email: 'Valor2',
+            // Asigna valores a otros campos según tus necesidades
+        });
 
+        // Guarda el documento en la colección
+        await nuevoDocumento.save();
+
+        console.log('Documento insertado en la colección "miColeccion"');
+
+        // Cierra la conexión a la base de datos cuando hayas terminado
+        mongoose.connection.close();
+    })
+    .catch((error) => {
+        console.error('Error de conexión a MongoDB:', error);
+        process.exit(1);
+    });
